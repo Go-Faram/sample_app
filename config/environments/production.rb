e6d348e27b1b5960fc0b08e3112f063f33d40768
Go-Faram <ruanjianzw@gmail.com>
2014-07-08 23:07:49 +0800

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+
   config.active_record.dump_schema_after_migration = false
+  config.force_ssl=true
 end
