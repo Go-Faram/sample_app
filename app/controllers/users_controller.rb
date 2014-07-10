2156651f71a237c0a000ac6b0526d51f5dd8dc78
Go-Faram <ruanjianzw@gmail.com>
2014-07-10 23:51:00 +0800

    Finish user edit,update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
-  	@user=User.find(params[:id])
+    @user=User.find(params[:id])
   end
 
   def new
@@ -11,17 +13,44 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-    flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success]="Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+
+  # Before filters
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user=User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
