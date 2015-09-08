b711c3f383d5d13165407cc1c0d5975340103139
zwei <ruanjianzw@gmail.com>
2015-09-08 18:14:27 +0800

    add valite

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
 
+  validates :title, exclusion: { in: %w(www us ca jp),
+                        message: "%{value} is reserved." }
+
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 5}
